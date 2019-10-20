f6fdcd67d7b099ce543a1874e6aad097d5530935
Georgie Ivanov <georgie@barimax.bg>
2019-10-20 11:21:22 +0300

    beta 2.1

--- Sources/CSCoreView/CSView/CSViewProtocolExtensions/CSViewRefOptionsExtension.swift
+++ Sources/CSCoreView/CSView/CSViewProtocolExtensions/CSViewRefOptionsExtension.swift
@@ -6,7 +6,50 @@
 //
 
 import Foundation
+import PerfectCRUD
+import PerfectMySQL
 
 public extension CSViewProtocol {
-    
+    var refs: [String:String] {
+        var res: [String:String] = [:]
+        for field in self.fields {
+            if let ref = field.ref {
+                res[field.name] = ref.registerName
+            }
+        }
+        return res
+    }
+    var refOptions: [String:CSRefOptionField] {
+        var result: [String:CSRefOptionField] = [:]
+        
+        for field in self.fields {
+            if let ref = field.ref  {
+                print("JORO: ViewProtocol: \(ref.options().count)")
+                let refOption: CSRefOptionField = CSRefOptionField(
+                    registerName: ref.registerName,
+                    options: ref.options(),
+                    isButton: false
+                )
+                result[field.name] = refOption
+            }
+        }
+        return result
+    }
+    var backRefs: [CSBackRefs] {
+        var res: [CSBackRefs] = []
+        for (_,entity) in CSRegister.store {
+            let view = entity.view()
+            for (field, ref) in view.refs {
+                if ref == Self.registerName {
+                    var backRefs = CSBackRefs()
+                    backRefs.registerName = type(of: view).registerName
+                    backRefs.singleName = view.singleName
+                    backRefs.pluralName = view.pluralName
+                    backRefs.formField = field
+                    res.append(backRefs)
+                }
+            }
+        }
+        return res
+    }
 }
