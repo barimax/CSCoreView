8f369aa7435d4dd388134bf400c72e1da6a0746c
Georgie Ivanov <georgie@barimax.bg>
2020-01-08 14:17:57 +0200

    Add public initialiser to CSDynamicEntityPropertyDescription

--- Sources/CSCoreView/CSView/CSDynamicEntityPropertyDescription.swift
+++ Sources/CSCoreView/CSView/CSDynamicEntityPropertyDescription.swift
@@ -6,12 +6,32 @@
 //
 
 public struct CSDynamicEntityPropertyDescription: Codable {
-    public let name: String,
-    fieldType: FieldType,
-    jsType: JSType,
-    colWidth: ColWidth,
-    required: Bool,
-    order: Int,
-    label: String,
-    refRegisterName: String?
+    public let name: String
+    public let fieldType: FieldType
+    public let jsType: JSType
+    public let colWidth: ColWidth
+    public let required: Bool
+    public let order: Int
+    public let label: String
+    public let refRegisterName: String?
+    
+    public init(
+        name: String,
+        refRegisterName: String? = nil,
+        fieldType: FieldType = FieldType.text,
+        jsType: JSType = .string,
+        colWidth: ColWidth = .normal,
+        required: Bool = true,
+        order: Int = 0,
+        label: String
+    ) {
+        self.name = name
+        self.label = label
+        self.refRegisterName = refRegisterName
+        self.fieldType = fieldType
+        self.jsType = jsType
+        self.colWidth = colWidth
+        self.required = required
+        self.order = order
+    }
 }
