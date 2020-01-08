e47591d8c98fad982780fd35d588e0583822f269
Georgie Ivanov <georgie@barimax.bg>
2020-01-08 14:35:24 +0200

    Revert CSDynamicFieldProtocol

--- Sources/CSCoreView/CSView/CSDynamicEntityProtocol.swift
+++ Sources/CSCoreView/CSView/CSDynamicEntityProtocol.swift
@@ -5,6 +5,6 @@
 //  Created by Georgie Ivanov on 2.01.20.
 //
 
-public protocol CSDynamicFieldProtocol {
-    static var fields: [CSPropertyDescription] { get }
+public protocol CSDynamicFieldProtocol: Codable {
+    static var fields: [CSDynamicEntityPropertyDescription] { get }
 }
